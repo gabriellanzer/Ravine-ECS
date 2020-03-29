be18c640df11061c4475fbf56236442d543d4914
Gabriel Lanzer Kannenberg <gabriellanzerlive@hotmail.com>
2020-03-28 21:40:34 -0300

    Project setup for cmake with LLVM and vscode CMAKE tools extension.

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include "BoundingSystem.hpp"
+#include "ComponentsManager.hpp"
+#include "GravitySystem.h"
+#include "MovementSystem.h"
+#include <chrono>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    ISystem *gravitySystem = new GravitySystem();
+    ISystem *movementSystem = new MovementSystem();
+    ISystem *boundingSystem = new BoundingSystem();
+    cout.setf(std::ios::fixed, std::ios::floatfield);
+    cout.precision(9);
+
+    ComponentsManager::createComponents<PositionComponent>({});
+    ComponentsManager::createComponents<VelocityComponent>({});
+    ComponentsManager::createComponents<VelocityComponent>({});
+    ComponentsManager::createComponents(PositionComponent{1, 1, 1}, VelocityComponent{0, 0, 0});
+    ComponentsManager::createComponents<VelocityComponent, PositionComponent>({0, 0, 0}, {2, 2, 2});
+    ComponentsManager::createComponents(PositionComponent{3, 3, 3}, VelocityComponent{0, 0, 0});
+    ComponentsManager::createComponents<VelocityComponent>({});
+    ComponentsManager::createComponents<PositionComponent>({});
+
+    while (true) {
+        // system("cls");
+
+        // Start Delta-time
+        static double dt = 0.03;
+        static auto mStartTime = std::chrono::system_clock::now();
+
+        // Update Systems
+        movementSystem->update(dt);
+        gravitySystem->update(dt);
+        boundingSystem->update(dt);
+
+        // Update Delta-time
+        static auto mEndTime = std::chrono::system_clock::now();
+        dt = std::chrono::duration_cast<std::chrono::nanoseconds>(mEndTime - mStartTime).count() * 1e-9;
+        cout << "Delta-time:" << dt << endl;
+    }
+    system("pause");
+    return 0;
+}
