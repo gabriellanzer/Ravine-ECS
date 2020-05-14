6688c9d96cad8e288289a51efdfe564a1850888f
Gabriel Lanzer Kannenberg <gabriellanzerlive@hotmail.com>
2020-05-14 01:06:06 -0300

    Added the 20.000.000 Component Test, commented MovementSystem cout.

--- src/MovementSystem.cpp
+++ src/MovementSystem.cpp
@@ -6,7 +6,7 @@ void MovementSystem::update(const double deltaTime, const size_t size, Component
     for (size_t i = 0; i < size; i++)
     {
         pos[i] += vel[i] * deltaTime;
-        std::cout << "pos " << pos[i] << "; vel " << vel[i] << std::endl;
+        // std::cout << "pos " << pos[i] << "; vel " << vel[i] << std::endl;
     }
-    system("cls");
+    // system("cls");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,23 @@ int main(int argc, char** argv)
     ISystem* movementSystem = new MovementSystem();
     ISystem* gravitySystem = new GravitySystem();
     ISystem* boundingSystem = new BoundingSystem();
-    ComponentsManager::createComponents(PositionComponent(0, 0, 0), VelocityComponent(0, 5, 0));
+    for (size_t i = 0; i < 20000000; i++)
+    {
+        ComponentsManager::createComponents(PositionComponent(i * 0.0001f, 0, 0), VelocityComponent(0, 1, 0));
+    }
 
     while(1)
     {
+        auto start = std::chrono::system_clock::now();
+        
         double deltaTime = 0.3;
         gravitySystem->update(deltaTime);
         boundingSystem->update(deltaTime);
         movementSystem->update(deltaTime);
+
+        auto end = std::chrono::system_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        std::cout << elapsed.count() * 0.000001 << "ms\n";
     }
     
     system("pause");
